ad157ffbd71c1aa20ca32854e85598e9087981c0
KyleMathew2003 <kyletmathew@gmail.com>
2024-03-03 03:28:39 -0500

    Initial Commit

---
+++ FitFlexer/FitFlexerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitFlexerApp.swift
+//  FitFlexer
+//
+//  Created by Kyle Mathew on 3/3/24.
+//
+
+import SwiftUI
+
+@main
+struct FitFlexerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
